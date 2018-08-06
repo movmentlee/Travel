3ae18d3800b6786782a83530e6d58ca4b6fe644a
movmentlee <movementgithub@outlook.com>
2018-08-07 00:20:35 +0800

    'project'

---
+++ src/components/Index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>Hello World</div>
+</template>
+
+<script>
+export default {
+  name:'Index'
+}
+</script>
+
+<style type="text/css" media="screen">
+	
+</style>
--- src/router.js
+++ src/router.js
@@ -3,21 +3,13 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
-
+// component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
