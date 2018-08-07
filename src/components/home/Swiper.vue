eb34d954e0cf9170aa7c8b2fc84c375f5951222f
movmentlee <movementgithub@outlook.com>
2018-08-08 05:00:03 +0800

    'change'

---
+++ src/components/home/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="swiper">
+	<swiper :options="swiperOption">
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+	      <img :src="item.imgUrl" />
+	    </swiper-slide>
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	</swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+         loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: require('@/assets/images/king_slanding.png')
+        },
+        {
+          id: '002',
+          imgUrl: require('@/assets/images/winterfell.png')
+        },
+        {
+          id: '003',
+          imgUrl: require('@/assets/images/casterly_rock.png')
+        },
+        {
+          id: '004',
+          imgUrl: require('@/assets/images/red_keep.png')
+        },
+        {
+          id: '005',
+          imgUrl: require('@/assets/images/the_wall.png')
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.swiper >>> .swiper-pagination-bullet-active
+  background-color:#ccc !important
+.swiper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.4%
+  background-color:#eee
+</style>
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <home-header/>
+    <home-swiper/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HomeHeader from '@/components/home/Header.vue'
+import HomeSwiper from '@/components/home/Swiper.vue'
 
 export default {
   name: 'home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
