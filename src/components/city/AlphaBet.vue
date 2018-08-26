85085f2d57f8235075fe18d88c59b9d0f41a740d
movmentlee <movementgithub@outlook.com>
2018-08-27 05:21:35 +0800

    finish city css

---
+++ src/components/city/AlphaBet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphaBet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../assets/styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      text-align: center
+      line-height: .4rem
+</style>
--- src/views/pages/City.vue
+++ src/views/pages/City.vue
@@ -2,22 +2,25 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphaBet></city-alphaBet>
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/city/Header.vue'
 import CitySearch from '@/components/city/Search.vue'
+import CityList from '@/components/city/List.vue'
+import CityAlphaBet from '@/components/city/AlphaBet.vue'
 
 export default {
   name: 'city',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphaBet
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-
-</style>
