cca35d8499c3255ba7c188a633e7d040fdb32ddc
movmentlee <movementgithub@outlook.com>
2018-09-04 11:23:17 +0800

    banner finish

---
+++ src/components/detail/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1807/2d/2df1b9b20956de54a3.img.jpg_600x330_a87318a1.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">九道谷漂流(AAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe796;</span>
+          27
+        </div>
+      </div>
+    </div>
+    <common-gallary 
+      :imgs="imgs" 
+      v-show="showGallary"
+      @close="handleGallaryClose"
+    ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from '@/common/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1807/e3/e31dbc1077d41e36a3.img.jpg_r_800x800_0f1c5b08.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    display: flex
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: .6rem
+    color: #fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-title
+      flex: 1
+      font-size: .32rem
+      padding: 0 .2rem
+    .banner-number
+      padding: 0 .4rem
+      line-height: .32rem
+      margin-top: .14rem
+      height: .32rem
+      border-radius: .2rem
+      background: rgba(0, 0, 0, .8)
+      font-size: .24rem
+      .banner-icon
+        font-size: .24rem
+</style>
